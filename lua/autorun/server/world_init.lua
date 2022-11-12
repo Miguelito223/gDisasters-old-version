8e9fb2646b149ad2429eaa9e4f483eabebd43fcc
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2022-11-12 17:54:12 +0100

    original archives

--- lua/autorun/server/world_init.lua
+++ lua/autorun/server/world_init.lua
@@ -95,7 +95,7 @@ hook.Add( "Initialize", "GdisastersSkyFix", function()
 
 if GetConVar("gdisasters_atmosphere"):GetInt() <= 0 then return end
 	
-if GetConVar("gdisasters_atmosphere"):GetInt() >= 1 then 
+if GetConVar("gdisasters_atmosphere"):GetInt() <= 1 then 
 
 
 	if #ents.FindByClass("env_skypaint")<1 then
@@ -120,7 +120,7 @@ hook.Add( "InitPostEntity", "gDisastersInitPostEvo", function()
 
 if GetConVar("gdisasters_atmosphere"):GetInt() <= 0 then return end
 	
-if GetConVar("gdisasters_atmosphere"):GetInt() >= 1 then 
+if GetConVar("gdisasters_atmosphere"):GetInt() <= 1 then 
 
 
 	local oldCleanUpMap = game.CleanUpMap
